a240cc06379fb9539a444d24118a23b6f6aafad3
Valetina-2608 <valusha2608@gmail.com>
2024-07-27 20:57:40 +0300

    Add questions list with answers

---
+++ script.js
@@ -0,0 +1,67 @@
+const questions = [
+    {
+        question:"The Great Wall of China is located in which country?",
+        answers:[
+            { text: "Japan", correct: false },
+            { text: "China", correct: true },
+            { text: "India", correct: false },
+            { text: "New Zealand", correct: false },
+        ]
+    },
+    {
+        question:"Petra, an ancient city known for its rock-cut architecture, is found in which country?",
+        answers:[
+            { text: "Egypt", correct: false },
+            { text: "Greece", correct: false },
+            { text: "Jordan", correct: true },
+            { text: "Turkey", correct: false },
+        ]
+    },
+    {
+        question:"Christ the Redeemer is a famous statue located in which city?",
+        answers:[
+            { text: "Buenos Aires", correct: false },
+            { text: "Mexico City", correct: false },
+            { text: "Santiago", correct: false },
+            { text: "Rio de Janeiro", correct: true },
+        ]
+    },
+    {
+        question:"Machu Picchu is an ancient Incan city located in which country?",
+        answers:[
+            { text: "Colombia", correct: false },
+            { text: "Peru", correct: true },
+            { text: "Chile", correct: false },
+            { text: "Bolivia", correct: false },
+        ]
+    },
+    {
+        question:"The Colosseum, an ancient amphitheater, is found in which city?",
+        answers:[
+            { text: "Rome", correct: true },
+            { text: "Paris", correct: false },
+            { text: "Madrid", correct: false },
+            { text: "Athens", correct: false },
+        ]
+    },
+    {
+        question:"The Taj Mahal, a white marble mausoleum, is located in which country?",
+        answers:[
+            { text: "Pakistan", correct: false },
+            { text: "Bangladesh", correct: false },
+            { text: "India", correct: true },
+            { text: "Sri Lanka", correct: false },
+        ]
+    },
+    {
+        question:"Chichen Itza, a large Mayan archaeological site, is located in which country?",
+        answers:[
+            { text: "Guatemala", correct: false },
+            { text: "Honduras", correct: false },
+            { text: "Belize", correct: true },
+            { text: "Mexico", correct: false },
+        ]
+    }
+
+
+]
