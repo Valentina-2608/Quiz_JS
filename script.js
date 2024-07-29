a77adad59edda383350b475a18f4561b0ff3e666
Valetina-2608 <valusha2608@gmail.com>
2024-07-29 23:03:54 +0300

    Add function showQuestion

--- script.js
+++ script.js
@@ -79,4 +79,22 @@ function startQuiz(){
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = 'Next question';
+    showQuestion();
+}
+
+
+function showQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + "." + currentQuestion.
+    question;
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerHTML = answer.text;
+        button.classList.add('btn');
+        answerButton.appendChild(button);
+    })
+
+
+
 }
